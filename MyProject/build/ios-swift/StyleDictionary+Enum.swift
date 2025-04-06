a97657890b007f50c684287261e7763309a9a264
Abdelrahman Faird <abdofareed661@gmail.com>
2025-04-06 13:20:39 +0200

    added the project fiels

---
+++ MyProject/build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,77 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let babyBlue = UIColor(red: 0.969, green: 0.980, blue: 1.000, alpha: 1)
+    public static let backgroundColor = UIColor(red: 0.969, green: 0.980, blue: 1.000, alpha: 1)
+    public static let bgImage = "url(../imgs/background-image.png)"
+    public static let bodyFontFamily = Inter
+    public static let bodyTextColor = UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1)
+    public static let borderRadiusMd = 16px
+    public static let borderRadiusSm = 8px
+    public static let borderWidthSm = 1px
+    public static let buttonBorderRadius = 16px
+    public static let buttonDisabledBackgroundColor = rgba(UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1), 0.5)
+    public static let buttonDisabledTextColor = UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1)
+    public static let buttonFontFamily = Inter
+    public static let buttonFontSize = 16px
+    public static let buttonFontWeight = Bold
+    public static let buttonLineHeight = 20px
+    public static let buttonMarginY = 20px
+    public static let buttonPaddingX = 20px
+    public static let buttonPaddingY = 14px
+    public static let fontSizeBody = 14px
+    public static let fontSizeH1 = 40px
+    public static let fontSizeH5 = 16px
+    public static let fontSizeH6 = 14px
+    public static let fontWeightBody = Regular
+    public static let fontWeightBold = Bold
+    public static let fontWeightRegular = Regular
+    public static let headingFontFamily = Inter
+    public static let headingFontSize = 40px
+    public static let headingFontWeight = Bold
+    public static let headingLineHeight = 48px
+    public static let headingPaddingBottom = 20px
+    public static let headingTextcolor = UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1)
+    public static let inputBackgroundColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let inputBorderColor = rgba(UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1), 0.5)
+    public static let inputBorderRadius = 8px
+    public static let inputBorderWidth = 1px
+    public static let inputFontFamily = Inter
+    public static let inputFontWeight = Regular
+    public static let inputFontsize = 14px
+    public static let inputGroupMarginTop = 40px
+    public static let inputLineHeight = 20px
+    public static let inputPaddingX = 20px
+    public static let inputPaddingY = 14px
+    public static let inputTextColor = UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1)
+    public static let inputWidth = 400px
+    public static let lineHeightBody = 20px
+    public static let lineHeightH1 = 48px
+    public static let lineHeightH5 = 20px
+    public static let lineHeightH6 = 20px
+    public static let loginContentPadding = 50px
+    public static let loginContentWidth = 500px
+    public static let logoHeight = 50px
+    public static let logoLight = "url(../imgs/logo-light.svg)"
+    public static let logoMarginBottom = 50px
+    public static let primaryFont = Inter
+    public static let royalBlue = UIColor(red: 0.259, green: 0.396, blue: 0.929, alpha: 1)
+    public static let sp14 = 14px
+    public static let sp20 = 20px
+    public static let sp40 = 40px
+    public static let sp50 = 50px
+    public static let steelBlue = UIColor(red: 0.482, green: 0.529, blue: 0.588, alpha: 1)
+    public static let sz400 = 400px
+    public static let sz50 = 50px
+    public static let sz500 = 500px
+    public static let textLinkColor = UIColor(red: 0.259, green: 0.396, blue: 0.929, alpha: 1)
+    public static let white = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
